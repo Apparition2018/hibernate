196aba9cb28e5617c163627a181fe950d91a8a87
Apparition2018 <88850180@163.com>
2020-01-03 16:44:46 +0800

    2020-01-03 16:44:38

--- src/test/java/com/ljh/SessionChangObjectStateTest.java
+++ src/test/java/com/ljh/SessionChangObjectStateTest.java
@@ -39,11 +39,11 @@ public class SessionChangObjectStateTest {
 
     /**
      * save()
-     * 1) 使一个临时对象变为持久化对象
-     * 2) 为对象分配ID.
-     * 3) 在 flush() 时会发送一条 INSERT 语句
-     * 4) 在 save() 之前设置的 ID 是无效的
-     * 5) 持久化对象的 ID 是不能被修改的
+     * 1. 使一个临时对象变为持久化对象
+     * 2. 为对象分配ID.
+     * 3. 在 flush() 时会发送一条 INSERT 语句
+     * 4. 在 save() 之前设置的 ID 是无效的
+     * 5. 持久化对象的 ID 是不能被修改的
      */
     @Test
     public void testSave() {
@@ -66,9 +66,7 @@ public class SessionChangObjectStateTest {
     }
 
     /**
-     * persist() 也会执行 INSERT 操作
-     *
-     * 和 save() 的区别：在调用 persist() 之前，对象已设置 ID，则不会执行 INSERT 语句且抛出异常
+     * persist() 和 save() 的区别：在调用 persist() 之前，对象已设置 ID，则不会执行 INSERT 语句且抛出异常
      */
     @Test
     public void testPersist() {
@@ -87,7 +85,7 @@ public class SessionChangObjectStateTest {
     
     @Test
     public void testGet() {
-        News news = session.get(News.class, 10);
+        News news = session.get(News.class, 1);
         System.out.println("news = " + news);
     }
 
@@ -96,13 +94,34 @@ public class SessionChangObjectStateTest {
      * 
      * 1. 执行 get() 会立即加载对象；而执行 load()，若不使用该对象，则不会立即执行查询操作，而返回一个代理对象
      *    get() 是立即检索，load() 是延迟检索
-     * 2. 若数据表中没有对应的记录，get() 返回 null，load() 抛出异常
-     * 3. 
+     * 2. load() 在需要初始化代理对象之前已经关闭了 Session 会抛出  LazyInitializationException
+     * 3. 若数据表中没有对应的记录
+     *    get() 返回 null，load() 初始化抛出异常
      */
     @Test
     public void testLoad() {
-        News news = session.load(News.class, 10);
+        News news = session.load(News.class, 1);
         System.out.println(news.getClass().getName()); // com.ljh.entity.News$HibernateProxy$3i8iYsPl
         System.out.println("news = " + news);
+    }
+
+    /**
+     * update()
+     * 1. 若更新一个持久化对象，不需要显式调用 update()，因为在调用 Transaction 的 commit() 时，会先执行 session 的 flush() 
+     * 2. 更新一个游离对象，需要显式调用 update()
+     */
+    @Test
+    public void testUpdate() {
+        News news = session.get(News.class, 1);
+        
+        transaction.commit();
+        session.close();
+        
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+                
+        news.setAuthor("Oracle");
+        
+        session.update(news);
     }
 }
